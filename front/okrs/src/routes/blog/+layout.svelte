89d8132434d04dc4a020fa2e09af2daafee8e0d8
dgjungleP <dgJungle_c@163.com>
2023-02-11 13:54:53 +0800

    style change

--- front/okrs/src/routes/blog/+layout.svelte
+++ front/okrs/src/routes/blog/+layout.svelte
@@ -12,7 +12,9 @@
 </script>
 
 <div class=" grid grid-cols-6">
-	<div class=" shadow-lg  fixed min-h-[40vh]  mt-[40px] ml-[20px] p-5 min-w-[14vw]">
+	<div
+		class=" shadow-lg border  fixed min-h-[40vh]  mt-[40px] ml-[20px] p-5 min-w-[14vw] before:block before:w-10 before:h-10    before:absolute before:-top-4 before:bg-[#FAE094] before:shadow "
+	>
 		<h1 class=" font-bold text-3xl text-center mb-2">Posts</h1>
 		{#each blogList as blog (blog)}
 			<a href={`/blog/posts/${blog.id}`}>{blog.title}</a>
