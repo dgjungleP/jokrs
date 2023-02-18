454ba6b690c77ab24679a9799c5bf8f91f290b2e
dgjungle_c <dgjungle_c@163.com>
2023-02-18 17:21:17 +0800

    home sunburst

---
+++ front/okrs/src/lib/data/sunburst.js
@@ -0,0 +1,473 @@
+export const data = [
+	{
+		name: '前端',
+		children: [
+			{
+				name: '脚手架',
+				children: [
+					{
+						name: '记录'
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'Vite',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习',
+						children: [
+							{
+								name: 'SelvteKit',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '掌握'
+					}
+				]
+			},
+			{
+				name: '语言',
+				children: [
+					{
+						name: '记录',
+						children: [
+							{
+								name: 'HTML5',
+								value: 1
+							},
+							{
+								name: 'CSS3',
+								value: 1
+							},
+							{
+								name: 'TypeScript',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'CSS',
+								value: 1
+							},
+							{
+								name: 'HTML',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习',
+						children: [
+							{
+								name: 'Javascript',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '掌握'
+					}
+				]
+			},
+			{
+				name: '框架',
+				children: [
+					{
+						name: '记录'
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'React.js',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习',
+						children: [
+							{
+								name: 'Sevlte.js',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '掌握'
+					}
+				]
+			},
+			{
+				name: '工具箱',
+				children: [
+					{
+						name: '记录'
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'Moment.js',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习'
+					},
+					{
+						name: '掌握'
+					}
+				]
+			}
+		]
+	},
+	{
+		name: '后端',
+		children: [
+			{
+				name: '框架',
+				children: [
+					{
+						name: '记录',
+						children: [
+							{
+								name: 'Netty',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'SpringCloud',
+								value: 1
+							},
+							{
+								name: 'Spring',
+								children: [
+									{
+										name: 'Spring Cache',
+										value: 1
+									}
+								]
+							},
+							{
+								name: 'Vert.x',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习',
+						children: [
+							{
+								name: 'Spring',
+								children: [
+									{
+										name: 'Spring Framework',
+										value: 1
+									}
+								]
+							},
+							{
+								name: 'SpringBoot',
+								value: 1
+							},
+							{
+								name: 'RxJava',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '掌握',
+						children: [
+							{
+								name: 'Mybatis',
+								value: 1
+							}
+						]
+					}
+				]
+			},
+			{
+				name: '语言',
+				children: [
+					{
+						name: '记录',
+						children: [
+							{
+								name: 'Java11',
+								value: 1
+							},
+							{
+								name: 'Java17',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '尝试',
+						children: [
+							{
+								name: 'Go',
+								value: 1
+							},
+							{
+								name: 'Python',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '学习',
+						children: [
+							{
+								name: 'Rust',
+								value: 1
+							}
+						]
+					},
+					{
+						name: '掌握',
+						children: [
+							{
+								name: 'Java8',
+								value: 1
+							}
+						]
+					}
+				]
+			},
+			{
+				name: '工具包',
+				children: [
+					{
+						name: '记录',
+						children: []
+					},
+					{
+						name: '尝试',
+						children: []
+					},
+					{
+						name: '学习',
+						children: []
+					},
+					{
+						name: '掌握',
+						children: [
+							{
+								name: 'Hutool',
+								value: 1
+							},
+							{
+								name: 'FastJson',
+								value: 1
+							},
+							{
+								name: 'Lombok',
+								value: 1
+							},
+							{
+								name: 'Swagger',
+								value: 1
+							},
+							{
+								name: 'TkMybatis',
+								value: 1
+							},
+							{
+								name: 'POI',
+								value: 1
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		name: '数据库',
+		children: [
+			{
+				name: 'KV类型',
+				children: [
+					{
+						name: '记录',
+						children: [{ name: 'ETCD', value: 1 }]
+					},
+					{
+						name: '尝试',
+						children: [
+							{ name: 'Zookeeper', value: 1 },
+							{ name: 'Redis', value: 1 }
+						]
+					},
+					{
+						name: '学习',
+						children: []
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			},
+			{
+				name: '文档类型',
+				children: [
+					{
+						name: '记录',
+						children: []
+					},
+					{
+						name: '尝试',
+						children: []
+					},
+					{
+						name: '学习',
+						children: [{ name: 'MongoDB', value: 1 }]
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			},
+			{
+				name: '关系型',
+				children: [
+					{
+						name: '记录',
+						children: [{ name: 'PostgreSql', value: 1 }]
+					},
+					{
+						name: '尝试',
+						children: [{ name: 'SqlServer', value: 1 }]
+					},
+					{
+						name: '学习',
+						children: [{ name: 'Mysql', value: 1 }]
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			},
+			{
+				name: '列式类型',
+				children: [
+					{
+						name: '记录',
+						children: [
+							{ name: 'HBase', value: 1 },
+							{ name: 'ClickHouse', value: 1 }
+						]
+					},
+					{
+						name: '尝试',
+						children: []
+					},
+					{
+						name: '学习',
+						children: []
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			}
+		]
+	},
+	{
+		name: '中间件',
+		children: [
+			{
+				name: '消息中间件',
+				children: [
+					{
+						name: '记录',
+						children: [
+							{ name: 'Kafka', value: 1 },
+							{ name: 'RabbitMq', value: 1 }
+						]
+					},
+					{
+						name: '尝试',
+						children: []
+					},
+					{
+						name: '学习',
+						children: []
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			},
+			{
+				name: '注册中心',
+				children: [
+					{
+						name: '记录',
+						children: [{ name: 'Kong', value: 1 }]
+					},
+					{
+						name: '尝试',
+						children: [
+							{ name: 'Nacos', value: 1 },
+							{ name: 'Eureka', value: 1 },
+							{ name: 'Consul', value: 1 }
+						]
+					},
+					{
+						name: '学习',
+						children: []
+					},
+					{
+						name: '掌握',
+						children: []
+					}
+				]
+			}
+		]
+	}
+];
+
+const baseTemple = {
+	name: '',
+	children: [
+		{
+			name: '记录',
+			children: []
+		},
+		{
+			name: '尝试',
+			children: []
+		},
+		{
+			name: '学习',
+			children: []
+		},
+		{
+			name: '掌握',
+			children: []
+		}
+	]
+};
